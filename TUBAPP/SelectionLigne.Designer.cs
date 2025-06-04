6fb2247b415587095b6fd590a109a7fc8fc727b7
Florent Bonvallet <florentbonvallet@gmail.com>
2025-06-04 09:12:15 +0200

    Refonte Page Selection Ligne

--- TUBAPP/SelectionLigne.Designer.cs
+++ TUBAPP/SelectionLigne.Designer.cs
@@ -58,16 +58,18 @@
             panelHeader.Controls.Add(btnProfil);
             panelHeader.Dock = DockStyle.Top;
             panelHeader.Location = new Point(0, 0);
+            panelHeader.Margin = new Padding(2, 2, 2, 2);
             panelHeader.Name = "panelHeader";
-            panelHeader.Size = new Size(561, 119);
+            panelHeader.Size = new Size(449, 95);
             panelHeader.TabIndex = 0;
             // 
             // picLogo
             // 
             picLogo.Image = Properties.Resources.logo;
             picLogo.Location = new Point(2, 2);
+            picLogo.Margin = new Padding(2, 2, 2, 2);
             picLogo.Name = "picLogo";
-            picLogo.Size = new Size(114, 116);
+            picLogo.Size = new Size(91, 93);
             picLogo.SizeMode = PictureBoxSizeMode.Zoom;
             picLogo.TabIndex = 0;
             picLogo.TabStop = false;
@@ -77,9 +79,10 @@
             lblAppName.AutoSize = true;
             lblAppName.Font = new Font("Segoe UI", 25F, FontStyle.Bold);
             lblAppName.ForeColor = Color.White;
-            lblAppName.Location = new Point(180, 32);
+            lblAppName.Location = new Point(144, 26);
+            lblAppName.Margin = new Padding(2, 0, 2, 0);
             lblAppName.Name = "lblAppName";
-            lblAppName.Size = new Size(223, 67);
+            lblAppName.Size = new Size(189, 57);
             lblAppName.TabIndex = 1;
             lblAppName.Text = "TUBAPP";
             // 
@@ -88,9 +91,10 @@
             btnProfil.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             btnProfil.Cursor = Cursors.Hand;
             btnProfil.Image = Properties.Resources.icone_profil;
-            btnProfil.Location = new Point(437, 5);
+            btnProfil.Location = new Point(350, 4);
+            btnProfil.Margin = new Padding(2, 2, 2, 2);
             btnProfil.Name = "btnProfil";
-            btnProfil.Size = new Size(79, 114);
+            btnProfil.Size = new Size(63, 91);
             btnProfil.SizeMode = PictureBoxSizeMode.Zoom;
             btnProfil.TabIndex = 2;
             btnProfil.TabStop = false;
@@ -101,9 +105,10 @@
             lblTitre.AutoSize = true;
             lblTitre.Font = new Font("Segoe UI", 22F, FontStyle.Bold);
             lblTitre.ForeColor = Color.FromArgb(33, 150, 243);
-            lblTitre.Location = new Point(150, 29);
+            lblTitre.Location = new Point(120, 23);
+            lblTitre.Margin = new Padding(2, 0, 2, 0);
             lblTitre.Name = "lblTitre";
-            lblTitre.Size = new Size(288, 120);
+            lblTitre.Size = new Size(242, 100);
             lblTitre.TabIndex = 0;
             lblTitre.Text = "Sélectionnez\nvotre ligne";
             lblTitre.TextAlign = ContentAlignment.MiddleCenter;
@@ -117,9 +122,10 @@
             panelMain.Controls.Add(lstTrajets);
             panelMain.Controls.Add(btnRetour);
             panelMain.Dock = DockStyle.Fill;
-            panelMain.Location = new Point(0, 119);
+            panelMain.Location = new Point(0, 95);
+            panelMain.Margin = new Padding(2, 2, 2, 2);
             panelMain.Name = "panelMain";
-            panelMain.Size = new Size(561, 624);
+            panelMain.Size = new Size(449, 499);
             panelMain.TabIndex = 1;
             // 
             // lblStations
@@ -127,9 +133,10 @@
             lblStations.AutoEllipsis = true;
             lblStations.BackColor = Color.FromArgb(236, 99, 92);
             lblStations.Font = new Font("Segoe UI", 14F, FontStyle.Bold);
-            lblStations.Location = new Point(80, 170);
+            lblStations.Location = new Point(30, 136);
+            lblStations.Margin = new Padding(2, 0, 2, 0);
             lblStations.Name = "lblStations";
-            lblStations.Size = new Size(280, 67);
+            lblStations.Size = new Size(258, 54);
             lblStations.TabIndex = 1;
             lblStations.Text = "Station 1 - Station 2";
             lblStations.TextAlign = ContentAlignment.MiddleLeft;
@@ -139,18 +146,20 @@
             panelHoraire.BackColor = Color.FromArgb(236, 99, 92);
             panelHoraire.Controls.Add(dtpDepartureTime);
             panelHoraire.Controls.Add(lblHoraire);
-            panelHoraire.Location = new Point(370, 170);
+            panelHoraire.Location = new Point(296, 136);
+            panelHoraire.Margin = new Padding(2, 2, 2, 2);
             panelHoraire.Name = "panelHoraire";
-            panelHoraire.Size = new Size(130, 67);
+            panelHoraire.Size = new Size(123, 54);
             panelHoraire.TabIndex = 2;
             // 
             // dtpDepartureTime
             // 
             dtpDepartureTime.CustomFormat = "HH:mm";
             dtpDepartureTime.Format = DateTimePickerFormat.Custom;
-            dtpDepartureTime.Location = new Point(0, 36);
+            dtpDepartureTime.Location = new Point(0, 29);
+            dtpDepartureTime.Margin = new Padding(2, 2, 2, 2);
             dtpDepartureTime.Name = "dtpDepartureTime";
-            dtpDepartureTime.Size = new Size(131, 31);
+            dtpDepartureTime.Size = new Size(123, 27);
             dtpDepartureTime.TabIndex = 1;
             // 
             // lblHoraire
@@ -158,8 +167,9 @@
             lblHoraire.AutoSize = true;
             lblHoraire.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
             lblHoraire.Location = new Point(0, 0);
+            lblHoraire.Margin = new Padding(2, 0, 2, 0);
             lblHoraire.Name = "lblHoraire";
-            lblHoraire.Size = new Size(114, 32);
+            lblHoraire.Size = new Size(94, 28);
             lblHoraire.TabIndex = 0;
             lblHoraire.Text = "Horaire :";
             // 
@@ -171,9 +181,10 @@
             lstTrajets.Font = new Font("Segoe UI", 8F, FontStyle.Bold);
             lstTrajets.FullRowSelect = true;
             lstTrajets.HeaderStyle = ColumnHeaderStyle.Nonclickable;
-            lstTrajets.Location = new Point(80, 240);
+            lstTrajets.Location = new Point(30, 192);
+            lstTrajets.Margin = new Padding(2, 2, 2, 2);
             lstTrajets.Name = "lstTrajets";
-            lstTrajets.Size = new Size(420, 300);
+            lstTrajets.Size = new Size(389, 240);
             lstTrajets.TabIndex = 3;
             lstTrajets.UseCompatibleStateImageBehavior = false;
             lstTrajets.View = View.Details;
@@ -207,9 +218,10 @@
             btnRetour.FlatStyle = FlatStyle.Flat;
             btnRetour.Font = new Font("Segoe UI", 16F, FontStyle.Bold);
             btnRetour.ForeColor = Color.White;
-            btnRetour.Location = new Point(158, 546);
+            btnRetour.Location = new Point(126, 437);
+            btnRetour.Margin = new Padding(2, 2, 2, 2);
             btnRetour.Name = "btnRetour";
-            btnRetour.Size = new Size(280, 60);
+            btnRetour.Size = new Size(224, 48);
             btnRetour.TabIndex = 4;
             btnRetour.Text = "Retour";
             btnRetour.UseVisualStyleBackColor = false;
@@ -230,18 +242,20 @@
             panelNav.Controls.Add(btnAccueil);
             panelNav.Controls.Add(btnReseau);
             panelNav.Dock = DockStyle.Bottom;
-            panelNav.Location = new Point(0, 743);
+            panelNav.Location = new Point(0, 594);
+            panelNav.Margin = new Padding(2, 2, 2, 2);
             panelNav.Name = "panelNav";
-            panelNav.Size = new Size(561, 81);
+            panelNav.Size = new Size(449, 65);
             panelNav.TabIndex = 2;
             // 
             // btnCarte
             // 
             btnCarte.Cursor = Cursors.Hand;
             btnCarte.Image = Properties.Resources.GPS;
-            btnCarte.Location = new Point(88, 6);
+            btnCarte.Location = new Point(70, 5);
+            btnCarte.Margin = new Padding(2, 2, 2, 2);
             btnCarte.Name = "btnCarte";
-            btnCarte.Size = new Size(56, 70);
+            btnCarte.Size = new Size(45, 56);
             btnCarte.SizeMode = PictureBoxSizeMode.Zoom;
             btnCarte.TabIndex = 0;
             btnCarte.TabStop = false;
@@ -251,9 +265,10 @@
             // 
             btnAccueil.Cursor = Cursors.Hand;
             btnAccueil.Image = Properties.Resources.homepng;
-            btnAccueil.Location = new Point(232, 0);
+            btnAccueil.Location = new Point(186, 0);
+            btnAccueil.Margin = new Padding(2, 2, 2, 2);
             btnAccueil.Name = "btnAccueil";
-            btnAccueil.Size = new Size(99, 81);
+            btnAccueil.Size = new Size(79, 65);
             btnAccueil.SizeMode = PictureBoxSizeMode.Zoom;
             btnAccueil.TabIndex = 1;
             btnAccueil.TabStop = false;
@@ -263,9 +278,10 @@
             // 
             btnReseau.Cursor = Cursors.Hand;
             btnReseau.Image = Properties.Resources.network;
-            btnReseau.Location = new Point(413, 4);
+            btnReseau.Location = new Point(330, 3);
+            btnReseau.Margin = new Padding(2, 2, 2, 2);
             btnReseau.Name = "btnReseau";
-            btnReseau.Size = new Size(74, 72);
+            btnReseau.Size = new Size(59, 58);
             btnReseau.SizeMode = PictureBoxSizeMode.Zoom;
             btnReseau.TabIndex = 2;
             btnReseau.TabStop = false;
@@ -273,13 +289,14 @@
             // 
             // frmSelectionLigne
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(561, 824);
+            ClientSize = new Size(449, 659);
             Controls.Add(panelMain);
             Controls.Add(panelHeader);
             Controls.Add(panelNav);
             FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(2, 2, 2, 2);
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "frmSelectionLigne";
@@ -298,7 +315,6 @@
             ((System.ComponentModel.ISupportInitialize)btnAccueil).EndInit();
             ((System.ComponentModel.ISupportInitialize)btnReseau).EndInit();
             ResumeLayout(false);
-
         }
 
         #endregion
